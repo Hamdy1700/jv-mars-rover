6f4f800d1085db57ddfaa1443f6d7898ef2f75ae
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-14 13:49:14 +0000

    refactor: InstructionParser.parse() to pass exceptional tests in InstructionParserTest

--- src/main/java/mars/parse/InstructionParser.java
+++ src/main/java/mars/parse/InstructionParser.java
@@ -5,7 +5,13 @@ import mars.Instruction;
 public class InstructionParser {
     public static Instruction parse(String input) {
         //remove anything that isn't the instruction
-        String instruction = input.replaceAll("[^LlRrMm]", "");
-        return Instruction.valueOf(instruction.toUpperCase());
+        String stringInstruction = input.replaceAll("[^LlRrMm]", "");
+        try {
+            return Instruction.valueOf(stringInstruction.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Input doesn't contain a valid instruction");
+        }
+        return null;
+        //handle illegal ARG E
     }
 }
