259053b38a1f91429ff3bb26e3940fd1902e6ae5
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-14 13:48:16 +0000

    feat: add InstructionParserTest.testExceptionalInputs()

--- src/test/java/mars/parse/InstructionParserTest.java
+++ src/test/java/mars/parse/InstructionParserTest.java
@@ -44,4 +44,27 @@ class InstructionParserTest {
 
     }
 
+    @Test
+    @DisplayName("When given exceptional inputs [blank, null, empty, not containing an instruction], output null")
+    void testExceptionalInput() {
+        var input1 = " ";
+        var input2 = "";
+        var input3 = "124234";
+        var input4 = "BHG";
+
+        var result1 = parse(input1);
+        var result2 = parse(input2);
+        var result3 = parse(input3);
+        var result4 = parse(input4);
+
+        assertAll(
+                () -> assertNull(result1),
+                () -> assertNull(result2),
+                () -> assertNull(result3),
+                () -> assertNull(result4),
+                () -> assertNull(null)
+        );
+
+    }
+
 }
