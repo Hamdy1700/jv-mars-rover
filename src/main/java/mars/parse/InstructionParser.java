46b683e4bbe51313f27b9b5e7589f377ff402f0d
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-14 15:30:07 +0000

    refactor: InstructionParser.parse() to output more than one instruction

--- src/main/java/mars/parse/InstructionParser.java
+++ src/main/java/mars/parse/InstructionParser.java
@@ -2,16 +2,22 @@ package mars.parse;
 
 import mars.Instruction;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstructionParser {
-    public static Instruction parse(String input) {
+    public static List<Instruction> parse(String input) {
         //remove anything that isn't the instruction
         String stringInstruction = input.replaceAll("[^LlRrMm]", "");
         try {
-            return Instruction.valueOf(stringInstruction.toUpperCase());
+            List<Instruction> instructions = new ArrayList<>();
+            for (int i = 0; i < stringInstruction.length(); i++){
+                instructions.add(Instruction.valueOf(String.valueOf(stringInstruction.charAt(i)).toUpperCase()));
+            }
+            return instructions;
         } catch (IllegalArgumentException e) {
             System.out.println("Input doesn't contain a valid instruction");
         }
         return null;
-        //handle illegal ARG E
     }
 }
