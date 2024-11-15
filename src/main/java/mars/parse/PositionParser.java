404ce8ead21a139046146623c98fd84a111bd16f
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 10:50:03 +0000

    feat: add PositionParser.parse()

--- src/main/java/mars/parse/PositionParser.java
+++ src/main/java/mars/parse/PositionParser.java
@@ -1,7 +1,35 @@
 package mars.parse;
 
+import mars.CardinalDirection;
+import mars.Position;
+
+import java.awt.Point;
+import java.util.IllegalFormatException;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class PositionParser {
 
+    public static Position parse(String input) throws IllegalArgumentException {
+        if (input == null) {
+            throw new IllegalArgumentException("Cannot parse null string");
+        }
 
+        Pattern positionFinder = Pattern.compile("^(\\d+)\\s(\\d+)\\s([NnEeSsWw])$");
+        Matcher matchPosition = positionFinder.matcher(input);
+
+        if (!matchPosition.matches()) {
+            throw new IllegalArgumentException("Cannot parse string with invalid format. Correct format: <int> <int> <CardinalDirection>");
+        }
+
+        int x = Integer.parseInt(matchPosition.group(1));
+        int y = Integer.parseInt(matchPosition.group(2));
+
+        Point coordinates = new Point(x, y);
+        CardinalDirection direction = CardinalDirection.valueOf(matchPosition.group(3).toUpperCase());
+
+        return new Position(coordinates, direction);
+
+    }
 
 }
