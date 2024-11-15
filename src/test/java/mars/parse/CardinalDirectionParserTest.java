eb2f89198482aca022aa8da5466ec6e1f3f5cc2f
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 01:25:22 +0000

    refactor: standard and exceptional tests

--- src/test/java/mars/parse/CardinalDirectionParserTest.java
+++ src/test/java/mars/parse/CardinalDirectionParserTest.java
@@ -60,17 +60,30 @@ class CardinalDirectionParserTest {
         var input3 = "124234";
         var input4 = "BHG";
 
-        var result1 = parse(input1);
-        var result2 = parse(input2);
-        var result3 = parse(input3);
-        var result4 = parse(input4);
-
         assertAll(
-                () -> assertNull(result1),
-                () -> assertNull(result2),
-                () -> assertNull(result3),
-                () -> assertNull(result4),
-                () -> assertNull(null)
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> parse(input1)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> parse(input2)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> parse(input2)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> parse(input3)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> parse(input4)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> parse(null)
+                )
         );
     }
 }
