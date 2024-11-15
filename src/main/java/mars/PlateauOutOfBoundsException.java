39a3b75d8f3bee99bbb256ec6701d5fab83cd080
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 14:07:26 +0000

    feat: add PlateauOutOfBoundsException

---
+++ src/main/java/mars/PlateauOutOfBoundsException.java
@@ -0,0 +1,11 @@
+package mars;
+
+public class PlateauOutOfBoundsException extends RuntimeException {
+
+    public PlateauOutOfBoundsException() {
+    }
+
+    public PlateauOutOfBoundsException(String message) {
+        super(message);
+    }
+}
