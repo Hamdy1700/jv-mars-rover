b0678070311f1216de3fb39de3c741bd01ef86ff
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 01:26:01 +0000

    refactor: standard and exceptional tests in InstructionParserTest

--- src/test/java/mars/parse/InstructionParserTest.java
+++ src/test/java/mars/parse/InstructionParserTest.java
@@ -21,6 +21,7 @@ class InstructionParserTest {
         var input5 = "124234M";
         var input6 = "512521m";
         var input7 = "LMMRrml";
+        var input8 = "LfMdMRbbrml";
 
         var expectedOutput1 = List.of(Instruction.L);
         var expectedOutput2 = List.of(Instruction.L);
@@ -29,6 +30,7 @@ class InstructionParserTest {
         var expectedOutput5 = List.of(Instruction.M);
         var expectedOutput6 = List.of(Instruction.M);
         var expectedOutput7 = List.of(Instruction.L, Instruction.M, Instruction.M, Instruction.R, Instruction.R, Instruction.M, Instruction.L);
+        var expectedOutput8 = List.of(Instruction.L, Instruction.M, Instruction.M, Instruction.R, Instruction.R, Instruction.M, Instruction.L);
 
         var result1 = parse(input1);
         var result2 = parse(input2);
@@ -37,6 +39,7 @@ class InstructionParserTest {
         var result5 = parse(input5);
         var result6 = parse(input6);
         var result7 = parse(input7);
+        var result8 = parse(input8);
 
         assertAll(
                 () -> assertEquals(expectedOutput1, result1),
@@ -45,7 +48,8 @@ class InstructionParserTest {
                 () -> assertEquals(expectedOutput4, result4),
                 () -> assertEquals(expectedOutput5, result5),
                 () -> assertEquals(expectedOutput6, result6),
-                () -> assertEquals(expectedOutput7, result7)
+                () -> assertEquals(expectedOutput7, result7),
+                () -> assertEquals(expectedOutput8, result8)
         );
 
     }
@@ -58,18 +62,32 @@ class InstructionParserTest {
         var input3 = "124234";
         var input4 = "BHG";
 
-        var result1 = parse(input1);
-        var result2 = parse(input2);
-        var result3 = parse(input3);
-        var result4 = parse(input4);
-
         assertAll(
-                () -> assertNull(result1),
-                () -> assertNull(result2),
-                () -> assertNull(result3),
-                () -> assertNull(result4),
-                () -> assertNull(null)
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> CardinalDirectionParser.parse(input1)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> CardinalDirectionParser.parse(input2)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> CardinalDirectionParser.parse(input2)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> CardinalDirectionParser.parse(input3)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class ,
+                        () -> CardinalDirectionParser.parse(input4)
+                ),
+
+                () -> assertThrows(IllegalArgumentException.class,
+                        () -> CardinalDirectionParser.parse(null)
+                )
         );
+
 
     }
 
