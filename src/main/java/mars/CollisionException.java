3ab199c179aaaa22f357918c19fb4464954cd225
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 14:07:40 +0000

    feat: add CollisionException

---
+++ src/main/java/mars/CollisionException.java
@@ -0,0 +1,11 @@
+package mars;
+
+public class CollisionException extends RuntimeException {
+
+    public CollisionException() {
+    }
+
+    public CollisionException(String message) {
+        super(message);
+    }
+}
