c7e9ec3e6c4b025ae915a3983f3f4db9363842e1
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 10:41:48 +0000

    feat: add moveForward() method in Rover class

--- src/main/java/mars/Rover.java
+++ src/main/java/mars/Rover.java
@@ -16,7 +16,7 @@ public class Rover {
 
     public void rotate(Instruction leftOrRight) throws IllegalArgumentException {
         if (!(leftOrRight == Instruction.R || leftOrRight == Instruction.L)) {
-            throw new IllegalArgumentException("Cannot rotate by moving forward! (Move instruction passed to rotate)");
+            throw new IllegalArgumentException("Instruction other than rotate passed to rotate()");
         }
         List<CardinalDirection> directions = List.of(N, E, S, W);
         int incrementValue = leftOrRight == Instruction.R ? 1 : -1;
@@ -24,6 +24,15 @@ public class Rover {
         indexOfNewDirection = indexOfNewDirection < 0 ? directions.size() - 1 : indexOfNewDirection % directions.size();
         CardinalDirection newDirection = directions.get(indexOfNewDirection);
         position.setFacing(newDirection);
+    }
+
+    public void moveForward() {
+        switch (position.getFacing()) {
+            case N -> position.translateCoordinates(0, 1);
+            case E -> position.translateCoordinates(1, 0);
+            case S -> position.translateCoordinates(0, -1);
+            case W -> position.translateCoordinates(-1, 0);
+        }
     }
 
     public Position getPosition() {
