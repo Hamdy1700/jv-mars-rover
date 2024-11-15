8883f027c5cec1a47813827a280f0ad528e3f452
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 01:29:09 +0000

    refactor: InstructionParser

--- src/main/java/mars/parse/InstructionParser.java
+++ src/main/java/mars/parse/InstructionParser.java
@@ -3,21 +3,26 @@ package mars.parse;
 import mars.Instruction;
 
 import java.util.ArrayList;
+import java.util.IllegalFormatException;
 import java.util.List;
 
 public class InstructionParser {
-    public static List<Instruction> parse(String input) {
+    public static List<Instruction> parse(String input) throws IllegalArgumentException {
+        if (input == null) {
+            throw new IllegalArgumentException("Cannot parse null string");
+        }
+
+        if (!input.matches(".*[LlRrMm].*")) {
+            throw new IllegalArgumentException("Cannot parse string without an instruction");
+        }
         //remove anything that isn't the instruction
         String stringInstruction = input.replaceAll("[^LlRrMm]", "");
-        try {
-            List<Instruction> instructions = new ArrayList<>();
-            for (int i = 0; i < stringInstruction.length(); i++){
-                instructions.add(Instruction.valueOf(String.valueOf(stringInstruction.charAt(i)).toUpperCase()));
-            }
-            return instructions;
-        } catch (IllegalArgumentException e) {
-            System.out.println("Input doesn't contain a valid instruction");
+
+        List<Instruction> instructions = new ArrayList<>();
+        for (int i = 0; i < stringInstruction.length(); i++){
+            instructions.add(Instruction.valueOf(String.valueOf(stringInstruction.charAt(i)).toUpperCase()));
         }
-        return null;
+        return instructions;
+
     }
 }
