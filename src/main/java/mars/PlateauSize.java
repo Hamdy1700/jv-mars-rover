f413dc86db73ae7f2efdae429c2816ef0bc29d04
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 11:48:02 +0000

    feat: add constructor to PlateuSize

--- src/main/java/mars/PlateauSize.java
+++ src/main/java/mars/PlateauSize.java
@@ -1,10 +1,19 @@
-import java.awt.*;
+package mars;
+
+import java.awt.Point;
 
 public class PlateauSize {
     private final Point TOP_RIGHT_COORDINATE;
     private final Point BOTTOM_LEFT_COORDINATE = new Point(0, 0);
-    private final int Y_MAX = (int) TOP_RIGHT_COORDINATE.getY();;
-    private final int X_MAX = (int) TOP_RIGHT_COORDINATE.getX();;
+    private final int Y_MAX;
+    private final int X_MAX;
     private final int Y_MIN = (int) BOTTOM_LEFT_COORDINATE.getY();
     private final int X_MIN = (int) BOTTOM_LEFT_COORDINATE.getX();
+
+    public PlateauSize(Point TOP_RIGHT_COORDINATE) {
+        this.TOP_RIGHT_COORDINATE = TOP_RIGHT_COORDINATE;
+        Y_MAX = (int) TOP_RIGHT_COORDINATE.getY();
+        X_MAX = (int) TOP_RIGHT_COORDINATE.getX();
+    }
+
 }
