30c0d22410f6ee310cba67a05a77c51ae7e6ed5d
Hamdy Abdalla <ha17007001@gmail.com>
2024-11-15 10:30:28 +0000

    feat: add getters & setters and translate() - just a wrapper for Point.translate()

--- src/main/java/mars/Position.java
+++ src/main/java/mars/Position.java
@@ -1,6 +1,35 @@
-import java.awt.*;
+package mars;
 
-class Position{
+import java.awt.Point;
+
+public class Position{
+
     private Point coordinate;
-    private CardinalDirection facing; // this type can be whatever your direction enum is called
+    private CardinalDirection facing;
+
+    public Position(Point coordinate, CardinalDirection facing) {
+        this.facing = facing;
+        this.coordinate = coordinate;
+    }
+
+    public CardinalDirection getFacing() {
+        return facing;
+    }
+
+    public Point getCoordinate() {
+        return coordinate;
+    }
+
+    public void setFacing(CardinalDirection facing) {
+        this.facing = facing;
+    }
+
+    public void translateCoordinates(int dx, int dy) {
+        coordinate.translate(dx, dy);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
